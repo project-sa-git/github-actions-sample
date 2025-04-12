f51acbe883610b657ccbe03391f3407568a37c36
mymac <trybook1901@gmail.com>
2025-04-13 03:12:26 +0900

    初回コミット

---
+++ ts-app/helle-world.ts
@@ -0,0 +1,9 @@
+/**
+ * このファイルはGitHub Actionsのサンプルプログラムです
+ *
+ * 実行すると「Hello MyActions」というメッセージをコンソールに出力します
+ * このプログラムはGitHub Actionsのワークフローで自動的に実行されます
+ */
+
+// コンソールにメッセージを出力する
+console.log("Hello MyActions");
